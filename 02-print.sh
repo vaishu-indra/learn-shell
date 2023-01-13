fd68c2f3199471e827367aec105dfeab384b7bdc
vaishnavi <indra.467@gmail.com>
2023-01-13 20:00:33 +0530

    shell scripting topics

--- 02-print.sh
+++ 02-print.sh
@@ -3,12 +3,12 @@ echo hello world
 echo "*****hello world****
 
 # color code syntax : echo -e "\e[colmMESSAGE\e[0m"
-#Red      - 31 - echo -e "\e[colmMESSAGE\e[0m"
-#Green    - 32 - echo -e "\e[colmMESSAGE\e[0m"
-#Yellow   - 33 - echo -e "\e[colmMESSAGE\e[0m"
-#Blue     - 34 - echo -e "\e[colmMESSAGE\e[0m"
-#Magenta  - 35 - echo -e "\e[colmMESSAGE\e[0m"
-#Cyan     - 36 - echo -e "\e[colmMESSAGE\e[0m"
+# Red      - 31 - echo -e "\e[colmMESSAGE\e[0m"
+# Green    - 32 - echo -e "\e[colmMESSAGE\e[0m"
+# Yellow   - 33 - echo -e "\e[colmMESSAGE\e[0m"
+# Blue     - 34 - echo -e "\e[colmMESSAGE\e[0m"
+# Magenta  - 35 - echo -e "\e[colmMESSAGE\e[0m"
+# Cyan     - 36 - echo -e "\e[colmMESSAGE\e[0m"
 
 echo -e "\e[31mMESSAGE\e[0m"
 echo -e "\e[32mMESSAGE\e[0m"
@@ -16,3 +16,4 @@ echo -e "\e[33mMESSAGE\e[0m"
 echo -e "\e[34mMESSAGE\e[0m"
 echo -e "\e[35mMESSAGE\e[0m"
 echo -e "\e[36mMESSAGE\e[0m"
+
