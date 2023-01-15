4a303073e9bfc611ed290a7fdc7adf38b490c3ac
vaishnavi <indra.467@gmail.com>
2023-01-15 15:55:38 +0530

    mongo

--- 05-special variable.sh
+++ 05-special variable.sh
@@ -1,11 +1,3 @@
-echo "following values from script"
-echo "0=$0"
-echo "1=$1"
-echo "2=$2"
-echo "*=$*"
-echo "@=$@"
-echo "#=$#"
-
 example() {
   echo "following values from function"
  echo "0=$0" # not applicable for function
@@ -16,4 +8,14 @@ example() {
  echo "#=$#"
 }
 
-example xyz 20
+echo "following values from script"
+echo "0=$0"
+echo "1=$1"
+echo "2=$2"
+echo "*=$*"
+echo "@=$@"
+echo "#=$#"
+
+
+
+example $*
